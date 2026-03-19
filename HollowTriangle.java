ec10dad033508e2a08228f429206f3d539447df9
Kamallochan Panigrahi <143154588+Kamalproject@users.noreply.github.com>
2026-03-19 22:49:45 +0530

    This code show you how to draw the hollow triangle.

---
+++ HollowTriangle.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class HollowTriangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter N: ");
+        int n=sc.nextInt();
+        
+        for(int i=1;i<=n;i++){
+            for(int s=1;s<=n-i;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                if(j==1||j==i||i==n){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
