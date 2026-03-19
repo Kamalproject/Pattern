a41f9d5dd0c3de8bfeaa3d34db20c7e22fd2773b
Kamallochan Panigrahi <143154588+Kamalproject@users.noreply.github.com>
2026-03-19 22:20:09 +0530

    This code show the inverted triangle. which is 180 degree inverted of normal triangle pattern

---
+++ Invertedtriangle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Invertedtriangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the n: ");
+        int n=sc.nextInt();
+        for(int i=n;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
